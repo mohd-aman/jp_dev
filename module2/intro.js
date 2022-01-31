42267fe61eff63af90c9b243e7768b4358ae4341
mohd-aman <saifiamaan445@gmail.com>
2022-01-31 21:38:34 +0530

    isPrime

--- module2/intro.js
+++ module2/intro.js
@@ -12,3 +12,33 @@ console.log(ch);
 
 let bool = true;//boolean
 console.log(bool)
+
+//loop
+for(let i=0;i<10;i++){
+    console.log(i);
+}
+
+//while
+
+// let num = 10;
+
+// while(num<20){
+//     console.log(num);
+//     num++;
+// }
+//Q. isPrime
+
+let n = 47
+let isPrime = true;
+
+for(let i=2;i*i<=n;i++){
+    if(n%i == 0){
+        isPrime = false;
+    }
+}
+if(isPrime == true){
+    console.log("It is a prime No")
+}
+else{
+    console.log("Not a prime")
+}
