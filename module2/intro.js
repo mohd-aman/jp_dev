b67049ad01bdd1e44af9ddc2d01b2a48fde7e713
mohd-aman <saifiamaan445@gmail.com>
2022-01-31 21:24:31 +0530

    js_intro

---
+++ module2/intro.js
@@ -0,0 +1,14 @@
+// print hello world
+console.log("Hello Pepcoders");
+
+let name = "Mohd Aman"; //string
+console.log(name)
+
+let num  = 1;//number
+console.log(num);
+
+let ch = 'a'//character
+console.log(ch);
+
+let bool = true;//boolean
+console.log(bool)
