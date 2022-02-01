77e6f1c12ebf8a9a0ed6f5508cd196e531954ebf
mohd-aman <saifiamaan445@gmail.com>
2022-02-01 23:30:23 +0530

    dir crud copyFile

--- module2/node/fs.js
+++ module2/node/fs.js
@@ -1,5 +1,7 @@
+//file system
 let fs = require('fs');
 // console.log(fs);
+let path = require('path');
 
 //read a file
 let content = fs.readFileSync('file.txt');
@@ -17,3 +19,30 @@ fs.appendFileSync('file.txt',"\nHow are you ?");
 //delete a file
 
 fs.unlinkSync('file2.txt');
+
+//read a directory
+
+let folderContent = fs.readdirSync(__dirname);
+console.log(folderContent);
+
+//check if exists or not
+let isExists = fs.existsSync('new_module');
+console.log(isExists);
+
+if(isExists == false){
+    //create a directory
+    fs.mkdirSync('new_module');
+}else{
+    // delete a directory
+    fs.rmdirSync('new_module');
+}
+
+
+//copy a file
+
+// fs.copyFileSync(pathOfFileToBeCopied,pathOfDestination)
+let dest = '/home/mike/Desktop/jp_dev/module2/js';
+let fileName = 'file.txt';
+let destPath = path.join(dest,fileName);
+console.log(destPath);
+fs.copyFileSync('/home/mike/Desktop/jp_dev/module2/node/file.txt',destPath);
