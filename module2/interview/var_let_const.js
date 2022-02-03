2314b4caa7972ebe2db42d45da3fb4566a7fb897
mohd-aman <saifiamaan445@gmail.com>
2022-02-03 22:51:46 +0530

    var let const

---
+++ module2/interview/var_let_const.js
@@ -0,0 +1,14 @@
+// let a;
+// a = 10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+// let a = 30; /// re-declaration not possible
+// console.log(a);
+
+
+// console.log(a);   //can't access before initialization
+// let a;
+
+const b = 10;
+console.log(b);
