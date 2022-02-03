af756d1238b19d7c4a0ed11852c5215b54872eb1
mohd-aman <saifiamaan445@gmail.com>
2022-02-03 22:19:42 +0530

    execution context

---
+++ module2/interview/executionContext.js
@@ -0,0 +1,27 @@
+//first part
+
+var a = 20;
+
+function fn(){
+    var a = 30;
+    console.log(a);
+}
+
+fn();
+
+//second part
+
+// console.log(a);
+// var a;
+// console.log(a);
+// a = 10;
+// console.log(a);
+
+
+//third part 
+
+// fn();
+
+// function fn(){
+//     console.log("Called before declaration");
+// }
